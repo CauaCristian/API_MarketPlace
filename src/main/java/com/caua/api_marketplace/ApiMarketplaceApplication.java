5f5d9a80aec7297b20da5d5b92cf5b512e806279
Cauã Cristian Inocêncio <125278407+CauaCristian@users.noreply.github.com>
2025-02-22 04:24:04 -0300

    att

--- src/main/java/com/caua/api_marketplace/ApiMarketplaceApplication.java
+++ src/main/java/com/caua/api_marketplace/ApiMarketplaceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiMarketplaceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiMarketplaceApplication.class, args);
     }
-
 }

4506e8e825b2aea426b1c3dc425828ea88d8f797
Cauã Cristian Inocêncio <125278407+CauaCristian@users.noreply.github.com>
2025-02-22 04:23:38 -0300

    att

--- src/main/java/com/caua/api_marketplace/Services/tokenService.java
+++ src/main/java/com/caua/api_marketplace/Services/tokenService.java
@@ -1,8 +1,33 @@
 package com.caua.api_marketplace.Services;
-
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.caua.api_marketplace.Models.User.UserModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class tokenService {
-    
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(UserModel user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("api-MarketPlace")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(generateExpiration())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("error while generating token", exception);
+        }
+    }
+    private Instant generateExpiration() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }

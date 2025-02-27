f90e1d1c07c78e1a8a62b2122a091640f388a5e7
Cauã Cristian Inocêncio <125278407+CauaCristian@users.noreply.github.com>
2025-02-27 08:33:01 -0300

    att

--- src/main/java/com/caua/api_marketplace/Controllers/ReservationController.java
+++ src/main/java/com/caua/api_marketplace/Controllers/ReservationController.java
@@ -1,4 +1,22 @@
 package com.caua.api_marketplace.Controllers;
 
+import com.caua.api_marketplace.DTO.Generic.ResponseDTO;
+import com.caua.api_marketplace.DTO.Relation.ReservationDTO;
+import com.caua.api_marketplace.Services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/reservation")
 public class ReservationController {
+    @Autowired
+    ReservationService reservationService;
+    @PostMapping(value = "/create",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseDTO<ReservationDTO> createReservation(@RequestBody ReservationDTO reservationDTO) {
+        return reservationService.createReservation(reservationDTO);
+    }
 }
--- src/main/java/com/caua/api_marketplace/Services/ReservationService.java
+++ src/main/java/com/caua/api_marketplace/Services/ReservationService.java
@@ -1,5 +1,7 @@
 package com.caua.api_marketplace.Services;
 
+import com.caua.api_marketplace.DTO.Generic.ResponseDTO;
+import com.caua.api_marketplace.DTO.Relation.ReservationDTO;
 import com.caua.api_marketplace.Mappers.ReservationMapper;
 import com.caua.api_marketplace.Repository.Relation.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +14,8 @@ public class ReservationService {
     @Autowired
     private ReservationMapper  reservationMapper;
 
-    
+    public ResponseDTO<ReservationDTO> createReservation(ReservationDTO reservationDTO) {
+        reservationRepository.save(reservationMapper.reservationDTOToReservationModel(reservationDTO));
+        return new ResponseDTO<ReservationDTO>("Reserva criado com sucesso",false,reservationDTO);
+    }
 }

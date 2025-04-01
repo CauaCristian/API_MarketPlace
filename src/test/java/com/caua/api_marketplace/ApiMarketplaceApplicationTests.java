17ddb5fbb3dda082717011c3d20d7bf93451105a
Cauã Cristian Inocêncio <125278407+CauaCristian@users.noreply.github.com>
2025-04-01 18:08:24 -0300

    att

--- src/test/java/com/caua/api_marketplace/ApiMarketplaceApplicationTests.java
+++ src/test/java/com/caua/api_marketplace/ApiMarketplaceApplicationTests.java
@@ -1,13 +1,6 @@
 package com.caua.api_marketplace;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiMarketplaceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
